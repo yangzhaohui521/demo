c9b661ea4b8854ab11671a5e959a352221d30fbd
yangchaohui <1712233212@qq.com>
2018-12-13 09:45:22 +0800

    测试

---
+++ src/main/java/com/demo/entity/User.java
@@ -0,0 +1,21 @@
+package com.demo.entity;
+
+/**
+ * @author yangchaohui
+ * @date 2018/12/12 15:46
+ */
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
